f787c7ca2933faeec8064c0d10db21287a580ab5
LuisTBH <luisgarciatbh@gmail.com>
2017-03-21 00:50:06 +0100

    Subiendo version buena de Objetos.java

--- prj-objetos/src/hola/Objetos.java
+++ prj-objetos/src/hola/Objetos.java
@@ -70,17 +70,24 @@ class RectanguloColor extends Rectangulo {
 		super(p, ancho, alto);
 		this.color = color;
 	}
+	
+/*	public RectanguloColor(int origenx, int origeny, int ancho, int alto, int color) {
+		origen = new Punto(origenx, origeny);
+		this.ancho = ancho;
+		this.alto = alto;
+		this.color = color;
+	}*/
 
 	public String toString() {
 		// EJERCICIO: Sobrecargue este método para que incluya en el String
 		// también el color
+		// COMPLETADO!!!!!
 		// .....................
-		// .....................
-		return "(Origen: {" + Integer.toString(origen.x) + "," + Integer.toString(origen.y) + "}, Final: {"
-				+ Integer.toString(origen.x + ancho) + "," + Integer.toString(origen.y + alto) + "}, Color: {" + Integer.toString(color) + "})";
+		return super.toString() + ", Color: " + Integer.toString(color) + "";
+		//return "(Origen: {" + Integer.toString(origen.x) + "," + Integer.toString(origen.y) + "}, Final: {"
+		//+ Integer.toString(origen.x + ancho) + "," + Integer.toString(origen.y + alto) + "}, Color: " + color + ")";
 	}
 }
-
 public class Objetos {
 	static Rectangulo R;
 	static RectanguloColor RC;
@@ -102,6 +109,10 @@ public class Objetos {
 		// atributo de color.
 		// .....................
 		// .....................
+		Punto puntoColor = new Punto(i[0],i[1]);
+		int numeroColor = Integer.parseInt(args[4]);
+		RC = new RectanguloColor(puntoColor, i[2], i[3],numeroColor);		
+		
 
 		System.out.println("Nombre de la clase de R: " + R.getClass().getName());
 		System.out.println("Descripción de la clase de R: " + Rectangulo.descripcion());
@@ -111,12 +122,22 @@ public class Objetos {
 
 		// EJERCICIO: Invoque el método mover(10,10) sobre R
 		// .....................
+		
+		R.mover(10, 10);
 		// EJERCICIO: Invoque el método toString sobre R y RC y visualicelos por
 		// pantalla el resultado
 		// .....................
 		// .....................
+		System.out.println("Despues de moverse R");
+		System.out.println("R: " + R.toString());
+		System.out.println("RC: " + RC.toString());
 
 		// PREGUNTA: Se ha "movido" R? y RC? Debería haberse "movido" RC?
 		// Explique convenientemente este aspecto.
+		
+		//No deberia haberse movido RC porque son dos instancias de objetos distintas, aunque la clase
+		//RectanguloColor extienda de Rectangulo no quiere decir que sean el mismo objeto. Sino que uno hereda
+		//los atributos y métodos de la clase padre, pudiendo tener otros constructores y añadiendo nuevos métodos
+		//y atributos.
 	}
 }
